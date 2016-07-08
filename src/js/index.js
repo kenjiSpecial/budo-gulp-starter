03125d1b8d9060951f67869903a3cdc9f85aa640
Kenji Saito <k.saito.1985@gmail.com>
2016-07-08 08:56:59 -0700

    made vive controller model.

--- src/js/index.js
+++ src/js/index.js
@@ -15,6 +15,11 @@ var room, controls, controller1, controller2;
 var effect;
 var light;
 
+var testScene, testCamera;
+var vivePath = './assets/vr_controller_vive_1_5.obj';
+var loader = new THREE.OBJLoader()
+var renderer;
+
 require('domready')(() => {
     init();
     loop();
@@ -24,47 +29,37 @@ function init() {
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(
+    testCamera = new THREE.PerspectiveCamera(
         70,
         window.innerWidth / window.innerHeight,
         0.1, 10000)
-    camera.position.z = 0
-    scene.add(new THREE.Mesh(
-        new THREE.BoxGeometry(6, 6, 6, 10, 10, 10),
-        new THREE.MeshBasicMaterial({color: 0x202020, wireframe: true})
-    ))
+    testCamera.position.y = 1;
+    testCamera.lookAt(new THREE.Vector3(0, 0, 0));
 
+    testScene = new THREE.Scene();
 
-    // scene.add(new THREE.HemisphereLight(0xffffff, 0xffffff, 1.0))
+    loader.load(vivePath, function (object) {
+        var loader = new THREE.TextureLoader()
+        var model = object.children[0]
+        model.material.color = new THREE.Color(1, 1, 1);
+        model.material.map = loader.load('./assets/onepointfive_texture.png')
+        model.material.specularMap = loader.load('./assets/onepointfive_spec.png')
+
+        testScene.add(object);
+    });
+
+    testScene.add(new THREE.HemisphereLight(0xffffff, 0xffffff, 1.0))
     light = new THREE.DirectionalLight(0xffffff);
     light.position.set(10, 10, 10).normalize()
     scene.add(light)
 
 
-    var renderer = new THREE.WebGLRenderer({antialias: true})
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setClearColor(0x101010)
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement)
-
-    controls = new THREE.VRControls(camera)
-    controls.standing = true
-
-    var controller = new ViveController(0, controls)
-    scene.add(controller)
-
-    var controller2 = new ViveController(1, controls)
-    scene.add(controller2)
-
-    
-
-    effect = new THREE.VREffect( renderer );
-
-    if ( WEBVR.isAvailable() === true ) {
-
-        document.body.appendChild( WEBVR.getButton( effect ) );
-
-    }
 
     window.addEventListener( 'resize', onWindowResize, false );
 
@@ -85,9 +80,8 @@ function loop() {
 }
 
 function render(){
-    controls.update();
 
-    effect.render(scene, camera);
+    renderer.render(testScene, testCamera);
 }
 
 
