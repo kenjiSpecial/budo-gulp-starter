744610076e9bd03b138e2937556c169e3945469c
Kenji <kenji@watsondg.com>
2016-07-07 20:33:58 -0700

    make new branch and add web vr starter and boiler plate.

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,93 @@
 const glslify = require('glslify');
+const raf = require('raf');
+window.THREE = require('three');
 
-const THREE = require('three');
+const WEBVR = require('./vendors/three/WebVR');
+require('./vendors/three/effects/VREffect');
+require('./vendors/three/controls/VRControls');
+// require('./vendors/three/ViveController');
+
+var ViveController = require('three-vive-controller')(THREE, './')
 
 var camera, scene, renderer;
 var geometry, shaderMaterial, mesh;
-
-function init() {
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 1000;
-
-    scene = new THREE.Scene();
-    geometry = new THREE.BoxGeometry(200, 200, 200);
-    shaderMaterial = new THREE.ShaderMaterial({
-        vertexShader: glslify('./shaders/shader.vert'),
-        fragmentShader: glslify('./shaders/shader.frag')
-    });
-
-    mesh = new THREE.Mesh(geometry, shaderMaterial);
-    scene.add(mesh);
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0x000000);
-
-    document.body.appendChild(renderer.domElement);
-}
-
-function loop() {
-    requestAnimationFrame(loop);
-
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
-
-    renderer.render(scene, camera);
-}
+var room, controls, controller1, controller2;
+var effect;
+var light;
 
 require('domready')(() => {
     init();
     loop();
 });
+
+function init() {
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        0.1, 10000)
+    camera.position.z = 0
+    scene.add(new THREE.Mesh(
+        new THREE.BoxGeometry(6, 6, 6, 10, 10, 10),
+        new THREE.MeshBasicMaterial({color: 0x202020, wireframe: true})
+    ))
+
+
+    // scene.add(new THREE.HemisphereLight(0xffffff, 0xffffff, 1.0))
+    light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(10, 10, 10).normalize()
+    scene.add(light)
+
+
+    var renderer = new THREE.WebGLRenderer({antialias: true})
+    renderer.setClearColor(0x101010)
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    controls = new THREE.VRControls(camera)
+    controls.standing = true
+
+    var controller = new ViveController(0, controls)
+    scene.add(controller)
+
+    var controller2 = new ViveController(1, controls)
+    scene.add(controller2)
+
+    
+
+    effect = new THREE.VREffect( renderer );
+
+    if ( WEBVR.isAvailable() === true ) {
+
+        document.body.appendChild( WEBVR.getButton( effect ) );
+
+    }
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+}
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    effect.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function loop() {
+    raf(loop);
+
+    render();
+}
+
+function render(){
+    controls.update();
+
+    effect.render(scene, camera);
+}
+
+
